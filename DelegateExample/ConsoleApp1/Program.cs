ad2792ef9a214c74e96e99f4cd8ee1320a83ed74
Denis_Tihonov <ndenmru@gmail.com>
2025-07-11 21:23:19 +0300

    Create Project DelegateExample

---
+++ DelegateExample/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        delegate void Message(); // Объявляем делегат
+        static void Main(string[] args)
+        {
+            // // Присваиваем этой переменной адрес метода
+            Message message1 = Welcome.Print;
+            Message message2 = new Hello().Display;
+
+            // Вызываем метод
+            message1(); // Welcome
+            message2(); // Привет
+        }
+    }
+
+    class Hello
+    {
+        public void Display() => Console.WriteLine("Привет");
+    }
+    class Welcome
+    {
+        public static void Print() => Console.WriteLine("Welcome");
+    }
+}
