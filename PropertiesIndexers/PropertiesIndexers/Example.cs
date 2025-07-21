e9bf747939058994e45132eee3385487ad01ca3a
Denis_Tihonov <ndenmru@gmail.com>
2025-07-21 12:09:10 +0300

    Create project PropertiesIndexers

---
+++ PropertiesIndexers/PropertiesIndexers/Example.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesIndexers
+{
+    public class Example
+    {
+        // instance fields
+        private int _id;
+        private string _name;
+        private string _job;
+        private double _salary;
+        private double _tax;
+
+        // instance property
+        public int Id 
+        { 
+            get
+            { 
+                return _id;
+            } 
+            set 
+            { 
+                if (value > 0)
+                {
+                    _id = value; 
+                }
+            }
+        }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Job { get { return _job; } set { _job = value; } }
+        
+        // readonly property
+        public double Salary { get { return _salary; } }
+        
+        // writeonly property
+        public double Tax 
+        { 
+            set 
+            { 
+                if (value >= 0)
+                {
+                    _tax = value;
+                }
+            } 
+        }
+
+        // static field
+        private static string _companyName;
+
+        // static property
+        public static string CompanyName 
+        { 
+            get 
+            { 
+                return _companyName; 
+            } 
+            set 
+            { 
+                if(value.Length <= 20)
+                {
+                    _companyName = value;
+                }
+            }
+        }
+    }
+}
---
+++ PropertiesIndexers/PropertiesIndexers/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PropertiesIndexers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PropertiesIndexers")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("14788eff-734b-4a8c-a874-de83ed92b2d5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
