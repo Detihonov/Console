e72894c4a23a3fb691a47939dbd0bef031030d93
Denis_Tihonov <ndenmru@gmail.com>
2025-07-22 14:08:07 +0300

    Modified file Example

--- PropertiesIndexers/PropertiesIndexers/Example.cs
+++ PropertiesIndexers/PropertiesIndexers/Example.cs
@@ -8,6 +8,14 @@ namespace PropertiesIndexers
 {
     public class Example
     {
+        // Modifier Metods
+        // 1.Private 2.Protected 3.Private protected
+        // 4.Internal 5.Protected internal 6.Public
+
+        // Modifier Fields
+        // 1.Static 2.Virtual 3.Abstract
+        // 4.Override 5.New 6.Sealed
+
         // instance fields
         private int _id;
         private string _name;
@@ -41,11 +49,18 @@ namespace PropertiesIndexers
         { 
             set 
             { 
-                if (value >= 0)
+                if (value >= 0 && value <= 100)
                 {
                     _tax = value;
                 }
             } 
+        }
+
+        // method CalculateTax
+        public double CalculateSalary()
+        {
+            double tax = Salary - _tax;
+            return tax;
         }
 
         // static field
@@ -65,6 +80,38 @@ namespace PropertiesIndexers
                     _companyName = value;
                 }
             }
+        }
+
+        // automatic properties
+        public string NativePlace { get; set; }
+
+        // default constructor
+        public Example()
+        {
+            _salary = 1000;
+        }
+
+        // instance constructor
+        public Example(int id, string name, string job)
+        {
+            _id=id;
+            _name=name;
+            _job=job;
+            _salary= 1000;
+        }
+
+        // constructor 2
+        public Example(int id, string name)
+        {
+            _id=id;
+            _name=name;
+            _salary = 1000;
+        }
+
+        // static constructor
+        static Example()
+        {
+            _companyName = "ABC Industries";
         }
     }
 }
--- PropertiesIndexers/PropertiesIndexers/Program.cs
+++ PropertiesIndexers/PropertiesIndexers/Program.cs
@@ -10,6 +10,55 @@ namespace PropertiesIndexers
     {
         static void Main(string[] args)
         {
+            // create three objects for Example
+            Example example = new Example();
+            Example example2 = new Example();
+            Example example3 = new Example();
+
+            example.Id = 1;
+            example.Name = "Scott";
+            example.Job = "Manager";
+            example.Tax = 13;
+            example.NativePlace = "Moskov";
+
+            example2.Id = 2;
+            example2.Name = "Allen";
+            example2.Job = "Asst.Manager";
+            example2.Tax = 50;
+            example2.NativePlace = "New Delhi";
+
+            example3.Id = 3;
+            example3.Name = "Anna";
+            example3.Job = "Clerk";
+            example3.Tax = 15;
+            example3.NativePlace = "New York";
+
+            // display fields
+            Console.WriteLine(Example.CompanyName);
+            Console.WriteLine("\nFirst Example: ");
+            Console.WriteLine(example.Id);
+            Console.WriteLine(example.Name);
+            Console.WriteLine(example.Job);
+            Console.WriteLine(example.Salary);
+            Console.WriteLine(example.CalculateSalary());
+            Console.WriteLine(example.NativePlace);
+            Console.WriteLine();
+
+            Console.WriteLine("\nSecond Example: ");
+            Console.WriteLine(example2.Id);
+            Console.WriteLine(example2.Name);
+            Console.WriteLine(example2.Job);
+            Console.WriteLine(example2.Salary);
+            Console.WriteLine(example2.NativePlace);
+            Console.WriteLine();
+
+            Console.WriteLine("\nThird Example: ");
+            Console.WriteLine(example3.Id);
+            Console.WriteLine(example3.Name);
+            Console.WriteLine(example3.Job);
+            Console.WriteLine(example3.Salary);
+            Console.WriteLine(example3.NativePlace);
+            Console.WriteLine();
         }
     }
 }
