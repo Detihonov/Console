d5946df377860649c3b8c2b4058b998b49815a86
Denis_Tihonov <ndenmru@gmail.com>
2025-08-06 12:24:39 +0300

    Create project Operators

---
+++ Operators/Operators/Complex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public struct Complex
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex()
+            {
+                Real = a.Real + b.Real,
+                Imaginary = a.Imaginary + b.Imaginary
+            };
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex a = new Complex() { Real = 1, Imaginary = 2};
+            Complex b = new Complex() { Real = 4, Imaginary = 8};
+            Complex c = a + b;
+        }
+    }
+}
