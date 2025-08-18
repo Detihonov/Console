f416653a65c420dea43768cecb1093df02518aae
Denis_Tihonov <ndenmru@gmail.com>
2025-08-18 11:47:29 +0300

    feat:Add Clock Class

---
+++ EventsAll/EventsAll/Clock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsAll
+{
+    internal class Clock
+    {
+        private event Action Tick;
+        private System.Timers.Timer timer;
+
+        public Clock()
+        {
+
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += (s, e) => Tick.Invoke();
+        }
+
+        public void Print()
+        {
+            Tick += () => Console.WriteLine($"Тик {DateTime.Now}");
+            Start();
+            Console.ReadKey();
+            Stop();
+            Console.WriteLine("Таймер остановлен");
+        }
+
+        private void Start() => timer.Start();
+        private void Stop() => timer.Stop();
+    }
+}
---
+++ EventsAll/EventsAll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var clock = new Clock();
+            clock.Print();
+        }
+    }
+}
