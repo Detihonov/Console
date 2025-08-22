1d98cd73b984746b3a10ef54d0e0ed52d6e1cdea
Denis_Tihonov <ndenmru@gmail.com>
2025-08-22 11:27:24 +0300

    feat: Create Class Student

---
+++ EventsAll/EventsAll/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventsAll
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public void Exam(string task)
+        {
+            Console.WriteLine($"Student {LastName} solved the {task}");
+        }
+    }
+}
