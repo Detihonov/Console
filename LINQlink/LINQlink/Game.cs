ede8d5aeb70e46a89d91abe812bc43c5fa8d17e9
Denis_Tihonov <ndenmru@gmail.com>
2025-08-26 16:15:01 +0300

    feat: Update LINQ Comand

--- LINQlink/LINQlink/Game.cs
+++ LINQlink/LINQlink/Game.cs
@@ -31,6 +31,7 @@ namespace LINQlink
                 new Game { Title = "The Legend of Zelda", Genre = "Adventure", ReleaseYear = 1986, Rating = 9.5, Price = 60},
                 new Game { Title = "Super Mario Bros.", Genre = "Platformer", ReleaseYear = 2022, Rating = 9.2, Price = 50},
                 new Game { Title = "Elder Ring", Genre = "RPG", ReleaseYear = 2022, Rating = 9.8, Price = 70},
+                new Game { Title = "New World", Genre = "RPG", ReleaseYear = 2020, Rating = 9.7, Price = 65},
                 new Game { Title = "Stardew Valley", Genre = "Simulation", ReleaseYear = 2016, Rating = 9.0, Price = 15},
                 new Game { Title = "Tetris", Genre = "Puzzle", ReleaseYear = 1984, Rating = 8.9, Price = 10}
             };
@@ -45,7 +46,7 @@ namespace LINQlink
 
             // Where
             var sortGames = games.Where(game => game.Genre == "RPG").ToList();
-            Console.WriteLine("Where: ");
+            Console.WriteLine("\nWhere: ");
             foreach (var game in sortGames)
             {
                 Console.WriteLine(game.Title);
@@ -53,11 +54,11 @@ namespace LINQlink
 
             // Any
             var modernGamesExist = games.Any(game => game.ReleaseYear > 2000);
-            Console.WriteLine($"Are there any modern games? {modernGamesExist}");
+            Console.WriteLine($"\nAre there any modern games? {modernGamesExist}");
 
             // OrderBy
             var sorteredByYear = games.OrderBy(game => game.ReleaseYear).ToList();
-            Console.WriteLine("Order BY: ");
+            Console.WriteLine("\nOrder BY: ");
             foreach (var game in sorteredByYear)
             {
                 Console.WriteLine($"{game.Title} {game.ReleaseYear}");
@@ -65,10 +66,60 @@ namespace LINQlink
 
             // OrderBy Descending (по убыванию)
             var sprtedDescinding = games.OrderByDescending(game => game.ReleaseYear).ToList();
-            Console.WriteLine("Order BY Descending: ");
+            Console.WriteLine("\nOrder BY Descending: ");
             foreach (var game in sprtedDescinding)
             {
                 Console.WriteLine($"{game.Title} {game.ReleaseYear}");
+            }
+
+            // Average
+            var averagePrice = games.Average(x => x.Price);
+            Console.WriteLine($"\nAverage Game Price: {averagePrice}");
+
+            // Min
+            var minRating = games.Min(x => x.Rating);
+            Console.WriteLine($"Minimum Rating: {minRating}");
+
+            // Max
+            var maxRating = games.Max(y => y.Rating);
+            Console.WriteLine($"Maximum Rating: {maxRating}");
+
+            // First
+            var bestGame = games.First(z => z.Rating == maxRating);
+            Console.WriteLine($"Best Game: {bestGame.Title} {bestGame.Rating}");
+
+            // GroupBy
+            var gamesGroup = games.GroupBy(x => x.Genre);
+            Console.WriteLine();
+            foreach (var group in gamesGroup)
+            {
+                Console.WriteLine($"Genre: {group.Key}");
+
+                foreach (var game in group)
+                {
+                    Console.WriteLine(game.Title);
+                }
+            }
+
+            // WHERE & ORDERBY & SELECT
+            var budgetAdventureGame = games
+                .Where(x => x.Genre == "Adventure" && x.Price <= 60)
+                .OrderBy(x => x.Rating)
+                .Select(x => $"{x.Title} - {x.Price}");
+            Console.WriteLine();
+
+            foreach (var game in budgetAdventureGame)
+            {
+                Console.WriteLine(game);
+            }
+
+            // SKIP & TAKE
+            var padinatedGames = games.Skip(2).Take(4);
+            Console.WriteLine();
+
+            foreach (var game in padinatedGames)
+            {
+                Console.WriteLine(game.Title);
             }
         }
     }
